ef78f3267092cc69b718de84b22e8aa8e1796f53
Mac <mac@MacBook-Pro.local>
2017-11-27 13:32:07 -0700

    Added overtime hours requests to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ActiveRecord::Base
+  validates_presence_of :date, :rationale, :overtime_request
+
+  validates :overtime_request, numericality: { greater_than: 0.0 }
+
   enum status: { submitted: 0, approved: 1, rejected: 2 }
   belongs_to :user
   validates_presence_of :date, :rationale
