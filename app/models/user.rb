a56601622e49437aed55a543397f03b45b3f4ef3
Mac <mac@MacBook-Pro.local>
2017-11-28 00:10:03 -0700

    Generated audit log and updated audit log factory

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
   has_many :posts
+  has_many :audit_logs
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -7,11 +8,11 @@ class User < ApplicationRecord
 
   validates_presence_of :first_name, :last_name, :phone
 
-  validates :phone, length: { is: 10 }
-
   PHONE_REGEX = /\A[0-9]*\Z/
 
   validates_format_of :phone, with: PHONE_REGEX
+
+  validates :phone, length: { is: 10 }
 
   def full_name
     last_name.upcase + ", " + first_name.upcase
