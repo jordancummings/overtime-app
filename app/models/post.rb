3e839e9e965442c1bba1466f852ac8861e436e59
Mac <jordan.dale.cummings@gmail.com>
2017-12-18 11:54:05 -0700

    Updated css files to scss

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,8 +7,6 @@ class Post < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :date, :rationale
 
-  scope :posts_by, ->(user) { where(user_id: user.id) }
-
   after_save :update_audit_log
 
   private
